52e1b304f91dc381711d192f75ca4d4bee769dd0
Pietro Alkmin <pietro.alkmin@sou.inteli.edu.br>
2025-05-02 19:16:52 -0300

    fix: Npm start

--- server.js
+++ server.js
@@ -1,31 +1,13 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-// Configuração do EJS como mecanismo de visualização
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// Middleware para processar JSON e formulários
+// Middleware para processar JSON
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Middleware para servir arquivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Rotas
 const routes = require('./routes/index');
 app.use('/', routes);
-
-// Tratamento de erro 404
-app.use((req, res) => {
-  res.status(404).render('pages/page1', {
-    pageTitle: 'Página não encontrada',
-    content: '../pages/page1',
-    error: 'Página não encontrada'
-  });
-});
 
 // Inicializa o servidor
 app.listen(PORT, () => {
